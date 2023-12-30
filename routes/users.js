36bb9d2af3cb6cca65e342e2424d973a7dd80991
Nadim Hossain <70945427+NadimHossainMahmud@users.noreply.github.com>
2023-12-30 17:00:22 +0600

    first commit

---
+++ routes/users.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const plm = require("passport-local-mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/pinterest");
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String
+  },
+  posts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post'
+  }],
+  dp: {
+    type: String, // Assuming that the profile picture is stored as a URL or file path
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  fullname: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.plugin(plm);
+
+module.exports = mongoose.model('User', userSchema);
+
+
